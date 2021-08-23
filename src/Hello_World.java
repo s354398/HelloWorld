469e71d898206385b134b5f3925b5dc5ac7c83b9
s354398 <71755152+s354398@users.noreply.github.com>
2021-08-23 10:49:37 +0200

    Correction

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -2,7 +2,7 @@ public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println("Tall til ti");
+        System.out.println("Numbers from 1 to 10");
         for (int i=1; i <= 10; i++){
             System.out.println(i);
         }
