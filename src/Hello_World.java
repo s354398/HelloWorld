1189e592fc5a765d018d7997da8c49ab1f89247c
s354398 <71755152+s354398@users.noreply.github.com>
2021-08-23 10:44:03 +0200

    Creating first programm Hello world

---
+++ src/Hello_World.java
@@ -0,0 +1,10 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("Tall til ti");
+        for (int i=0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
