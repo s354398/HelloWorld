c1cec004bb882756910d2d2a09f8185977126f05
s354398 <71755152+s354398@users.noreply.github.com>
2021-08-23 11:06:24 +0200

    Correction

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -6,6 +6,6 @@ public class Hello_World {
         for (int i=1; i <= 10; i++){
             System.out.println(i);
         }
-        System.out.println("Completed");
+        System.out.println("Task Completed");
     }
 }
